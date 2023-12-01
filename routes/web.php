d1172184fc5e4a7ebd64c991c9c5783f5765270e
fauzan ahmad sihabudin <fauzan.a.s@students.esqbs.ac.id>
2023-12-01 14:31:30 +0700

    first reg

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('register', function () {
-    return view('register');
-});
-
-//tes pull
-
-Route::get('register', function () {
-    return view('register');
-});
-
-Route::get('register', function () {
-    return view('register');
+    return view('authentication/register');
 });
