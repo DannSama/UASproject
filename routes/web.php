1f242b29e3c464ee37ad72009d1c04ceec6b0e7c
Athaya Hanif <96409853+DannSama@users.noreply.github.com>
2023-12-01 14:15:13 +0700

    tes pull

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('register', function () {
     return view('register');
 });
+
+//tes pull
+
+Route::get('register', function () {
+    return view('register');
+});
+
+Route::get('register', function () {
+    return view('register');
+});
