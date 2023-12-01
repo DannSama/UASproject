fcc308fff71b5b6c8754b95ba1f45d2aff0cf2bc
fauzan ahmad sihabudin <fauzan.a.s@students.esqbs.ac.id>
2023-12-01 14:51:10 +0700

    controller,httpuser,register
    new

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return view('register');
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        auth()->login($user);
+
+        return redirect()->route('home');
+    }
+}
--- resources/views/authentication/register.blade.php
+++ resources/views/authentication/register.blade.php
@@ -1,33 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registrasi</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Registrasi</h1>
 
-namespace App\Http\Controllers;
+        <form action="{{ route('register') }}" method="post">
+            @csrf
 
-use App\Models\User;
-use Illuminate\Http\Request;
+            <div class="form-group">
+                <label for="name">Nama</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+                @error('name')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
 
-class RegisterController extends Controller
-{
-    public function index()
-    {
-        return view('register');
-    }
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
+                @error('email')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
 
-    public function store(Request $request)
-    {
-        $this->validate($request, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+                @error('password')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => bcrypt($request->password),
-        ]);
+            <div class="form-group">
+                <label for="password_confirmation">Konfirmasi Password</label>
+                <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                @error('password_confirmation')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
+            </div>
 
-        auth()->login($user);
-
-        return redirect()->route('home');
-    }
-}
+            <button type="submit" class="btn btn-primary">Daftar</button>
+        </form>
+    </div
