f986dd84657833278adc3f7f4975e5478c6cfc56
fadhilraihan23 <raihanfadhilah23@gmail.com>
2023-12-22 14:13:51 +0700

    register dan login

--- resources/views/authentication/login.blade.php
+++ resources/views/authentication/login.blade.php
@@ -33,16 +33,17 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
+                            <!-- <div class="col-lg-6"> -->
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
                                     <form class="user" action="index.html">
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" id="exampleUserName"
-                                                placeholder="User Name" maxlength="20" required>
+                                            <input type="text" class="form-control form-control-user" id="exampleUsername"
+                                                placeholder="Username" maxlength="20" required>
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
--- resources/views/authentication/register.blade.php
+++ resources/views/authentication/register.blade.php
@@ -28,8 +28,9 @@
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
-                    <div class="col-lg-7">
+                    <!-- <div class="col-lg-5 d-none d-lg-block bg-register-image"></div> -->
+                    <!-- <div class="col-lg-7"> -->
+                    <div class="col-lg-12">
                         <div class="p-5">
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
@@ -44,8 +45,8 @@
                                         placeholder="Email Address" maxlength="100" required>
                                 </div>
                                 <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="exampleUserName"
-                                        placeholder="User Name" maxlength="20" required>
+                                    <input type="text" class="form-control form-control-user" id="exampleUsername"
+                                        placeholder="Username" maxlength="20" required>
                                 </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
