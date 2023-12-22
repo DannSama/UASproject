fb15207cf731a1c1f9cfa27663bb231b6cf5b021
fadhilraihan23 <raihanfadhilah23@gmail.com>
2023-12-22 16:28:05 +0700

    repeat pass removed

--- resources/views/authentication/register.blade.php
+++ resources/views/authentication/register.blade.php
@@ -41,22 +41,23 @@
                                         placeholder="Full Name" maxlength="100" required>
                                 </div>
                                 <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                        placeholder="Email Address" maxlength="100" required>
-                                </div>
-                                <div class="form-group">
                                     <input type="number" class="form-control form-control-user" id="examplePhoneNumber"
                                         placeholder="Phone Number" maxlength="15" required>
                                 </div>
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail"
+                                        placeholder="Email Address" maxlength="100" required>
+                                </div>
                                 <div class="form-group row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                    <!-- <div class="col-sm-6 mb-3 mb-sm-0"> -->
+                                    <div class="col-sm-12 mb-3 mb-sm-0">
                                         <input type="password" class="form-control form-control-user"
                                             id="exampleInputPassword" placeholder="Password" minlength="8" maxlength="50" required>
                                     </div>
-                                    <div class="col-sm-6">
+                                    <!-- <div class="col-sm-6">
                                         <input type="password" class="form-control form-control-user"
                                             id="exampleRepeatPassword" placeholder="Repeat Password" minlength="8" maxlength="50" required>
-                                    </div>
+                                    </div> -->
                                 </div>
                                 <button type="submit" class="btn btn-primary btn-user btn-block">Register Account</button>
                                 <!-- <a href="login.html" class="btn btn-primary btn-user btn-block">
