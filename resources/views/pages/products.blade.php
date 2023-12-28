2f7a50a42418ef37e6682eb4c520f27afb23f0fd
Athaya Hanif <96409853+DannSama@users.noreply.github.com>
2023-12-28 10:01:56 +0700

    komit dlu
    iua

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -5,45 +5,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <title>Document</title>
+    <title>Product CRUD</title>
 </head>
 <body>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th>Nama Produk</th>
-            <th>Jenis Produk</th>
-            <th>Harga</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($products as $product)
-        <tr>
-            <td>{{ $product->nama_produk }}</td>
-            <td>{{ $product->jenis_produk }}</td>
-            <td>{{ $product->harga }}</td>
-            <td>
-                <form action="{{ route('products.destroy', $product->id_produk) }}" method="POST">
-                    <a href="{{ route('products.edit', $product->id_produk) }}" class="btn btn-primary">Edit</a>
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
- 
-<form action="{{ route('products.store') }}" method="POST">
-    @csrf
-    <div class="form-group">
-        <label for="nama_produk">Nama Produk</label>
-        <input type="text" class="form-control" id="nama_produk" name="nama_produk" required>
-    </div>
+<div class="container mt-5">
+    <h2>Product List</h2>
 
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Nama Produk</th>
+                <th>Jenis Produk</th>
+                <th>Harga</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($products as $product)
+            <tr>
+                <td>{{ $product->nama_produk }}</td>
+                <td>{{ $product->jenis_produk }}</td>
+                <td>{{ $product->harga }}</td>
+                <td>
+                    <form action="{{ route('products.destroy', $product->id_produk) }}" method="POST">
+                        <a href="{{ route('products.edit', $product->id_produk) }}" class="btn btn-primary">Edit</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <a href="{{ route('products.create') }}" class="btn btn-success">Add Product</a>
+    
+    <form action="{{ route('products.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="nama_produk">Nama Produk</label>
+            <input type="text" class="form-control" id="nama_produk" name="nama_produk" required>
+        </div>
+
+        <div class="form-group">
+            <label for="jenis_produk">Jenis Produk</label>
+            <!-- Add other form fields as needed -->
+            <input type="text" class="form-control" id="jenis_produk" name="jenis_produk" required>
+        </div>
+
+        <div class="form-group">
+            <label for="harga">Harga</label>
+            <input type="number" class="form-control" id="harga" name="harga" required>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
